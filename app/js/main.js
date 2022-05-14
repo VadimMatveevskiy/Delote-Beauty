4273ca2c258662876fe041dc686b5264ef4a263c
VadimMatveevskiy <matveevskiy4598@mail.ru>
2022-05-14 17:30:38 +0500

    added index.html

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,18 @@
 $(function(){
-
-  
+    
+    $('.top__slide').on('click', function(event){
+        event.preventDefault();
+        $('.top__slide').addClass('top__slide--active');
+        var id  = $(this).attr('href'),
+        top = $(id).offset().top;
+		$('body,html').animate({scrollTop: top}, 1500);
+    });
+    $(window).on('scroll',function(){
+        var height = $(window).scrollTop();
+        if(height > 1000){
+            $('.top__slide').removeClass('top__slide--active');
+        } else{
+        }
+    });
+    var mixer = mixitup('.ourWork__inner');
 })
